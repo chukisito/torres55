e8d8388ddc29880c5cabb48d2e87a6a38e5c9c91
edith espinoza <edith_espino@hotmail.com>
2016-03-04 01:47:22 -0500

    practica

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<font color="gree">
+<h1>lynn gavilan torres</h1>
+
+
+</font>
+	<img src="12781964_1699586620296824_387330342_n.jpg" alt="25">
+<h1>seccion:2tt55</h1>
+</body>
+</html>
